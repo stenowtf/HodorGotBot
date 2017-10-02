3b3a9c7dc83b6fe90f012b458eca6ef937e495e1
Stefano Canducci <steno@fastmail.fm>
2017-10-02 19:39:38 +0200

    tests lol

--- jest/modules/hodorTalk.spec.js
+++ jest/modules/hodorTalk.spec.js
@@ -1,13 +1,13 @@
 const hodorTalk = require('../../src/modules/hodorTalk');
 
 test('get generic response', () => {
-    expect(hodorTalk.getGenericResponse()).toContain('Hodor');
+    expect(hodorTalk.getGenericResponse()).toContain('...');
 });
 
 test('have a nice day response', () => {
-    expect(hodorTalk.getHaveANiceDay('Matteo')).toEqual('Have a nice day Matteo');
+    expect(hodorTalk.getHaveANiceDay('Matteo')).toEqual('...');
 });
 
 test('odor by hodor response', () => {
-    expect(hodorTalk.getOdorByHodor('lol')).toEqual('odor by hodor: lol');
+    expect(hodorTalk.getOdorByHodor('lol')).toEqual('...');
 });
